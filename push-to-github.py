9eb5e17f80184dac72352f06d28bf4a340fbd933
lyhue1991 <lyhue1991@163.com>
2022-07-17 12:46:13 +0800

    update einsum

--- push-to-github.py
+++ push-to-github.py
@@ -17,7 +17,7 @@
 
 # !git rm --cached  .ipynb_checkpoints/* 
 
-# !git commit -m "update " 
+# !git commit -m "update" 
 
 # !git remote add origin git@github.com:lyhue1991/eat_pytorch_in_20_days.git
 
